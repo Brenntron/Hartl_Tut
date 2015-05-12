3857b2ae7cc90329092f05b207857f75f44a542c
Brennan Willingham <brennanw22@gmail.com>
2015-05-11 20:10:21 -0500

    Changed the page routes so they don't say /static_pages/. Cus dat ugly

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
+  root          'static_pages#home'
 
-  get 'static_pages/help'
+  get 'help' => 'static_pages#help'
 
-  get 'static_pages/about'
+  get 'help' => 'static_pages#about'
 
-  get 'static_pages/contact'
+  get 'help' => 'static_pages#contact'
 
   resources :microposts
 
